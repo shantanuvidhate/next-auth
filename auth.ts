33b346ffc21b34b4604d825affe8ed0228a476e4
shantanuvidhate <shantanuvidhate00@gmail.com>
2024-02-16 14:24:56 +0530

    admin and user role schame change

--- auth.ts
+++ auth.ts
@@ -5,6 +5,17 @@ import { db } from "@/lib/db";
 
 
 export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
+    callbacks: {
+        async session({ token, session }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+            }
+            return session;
+        },
+        async jwt({ token }) {
+            return token;
+        }
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
     ...authConfig,
